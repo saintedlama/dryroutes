6c4c1021d20629ee89beb44a2679822992dc3b57
saintelama <christoph.walcher@gmail.com>
2014-05-02 16:43:50 +0200

    Upgrades to express 4

--- app.js
+++ app.js
@@ -1,42 +1,38 @@
 var express = require('express');
-var routes = require('./routes');
-var tasks = require('./routes/tasks');
-var http = require('http');
 var path = require('path');
-var app = express();
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-app.locals.appname = 'Express.js Todo App';
-
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(express.cookieParser());
-app.use(express.session({secret: '59B93087-78BC-4EB9-993A-A61FC844F6C9'}));
-app.use(express.csrf());
-
-app.use(require('less-middleware')({ src: __dirname + '/public', compress: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(function(req, res, next) {
-  res.locals._csrf = req.session._csrf;
-  return next();
-});
-
-app.use(app.router);
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+var less = require('less-middleware');
+var routes = require('./routes/index');
+var tasks = require('./routes/tasks');
 
 var loadCursor = require('./lib/load_cursor');
 var lookupView = require('./lib/lookup_view');
 var validate = require('./lib/validate');
 var success = require('./lib/success');
+
+var http = require('http');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(less({ src: __dirname + '/public', compress: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.locals.appname = 'Express.js Todo App';
 
 var validateAddTask = function(req, res, next) {
   if (!req.body || !req.body.name) return next(new Error('No data provided.'));
@@ -49,12 +45,39 @@ app.get('/tasks', loadCursor, lookupView, tasks.list);
 app.post('/tasks', validate(validateAddTask), loadCursor, lookupView, success, tasks.add);
 app.post('/tasks/complete', success, tasks.markAllCompleted);
 app.post('/tasks/:task_id', success, tasks.markCompleted);
-app.del('/tasks/:task_id', success, tasks.del);
-
+app.del('/tasks/:task_id', tasks.del);
 app.get('/tasks/completed', loadCursor, lookupView, tasks.completed);
 
-app.all('*', function(req, res){
-  res.send(404);
+
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 http.createServer(app).listen(app.get('port'), function(){
