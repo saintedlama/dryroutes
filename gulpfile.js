50396a89665e61432627f0085252b06b41bf5237
Christoph Walcher <c.walcher@diamonddogs.cc>
2014-05-12 16:32:27 +0200

    Fixes fonts and animations

--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,23 @@ gulp.task('clean-doc', function() {
     .pipe(p.clean());
 });
 
-// TODO: Base directory got wrong here!
-gulp.task('copy-assets', function() {
-  return gulp.src(['docgen/public/**', 'docgen/bower_components/**', 'docgen/public/docco.css'])
+gulp.task('copy-bower', function() {
+  return gulp.src('docgen/bower_components/**')
+    .pipe(gulp.dest('doc/bower_components'));
+});
+
+gulp.task('copy-docco', function() {
+  return gulp.src('docgen/public/**')
     .pipe(gulp.dest('doc/public'));
 });
 
-gulp.task('doc', ['copy-assets'], function() {
+
+gulp.task('copy', ['copy-bower', 'copy-docco'], function() {
+  return gulp.src(['docgen/public/**'])
+    .pipe(gulp.dest('doc/public'));
+});
+
+gulp.task('doc', ['copy'], function() {
   return gulp.src(['./routes/tasks.js'])
     .pipe(p.docco({  template: 'docgen/docco.jst' }))
     .pipe(gulp.dest('doc'));
