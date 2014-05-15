33c81ff1c886ea221585ae4e4b2478eac4df6c95
saintelama <christoph.walcher@gmail.com>
2014-05-15 20:05:14 +0200

    Removes console.log

--- middleware/render.js
+++ middleware/render.js
@@ -1,12 +1,8 @@
 module.exports = function() {
   return function(req, res, next){
     if (req.view && req.model) {
-      console.log('model and view are set for render', req.view, req.model);
-
       return res.render(req.view, req.model);
     }
-
-    console.log('model or view are NOT set, calling next',req.view, req.model);
 
     next();
   }
